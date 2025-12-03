058e2a6e341e0db27e1a53a67463694185f22879
Nagaraja P <nagaraja.palsamy@buildingblocks.la>
2025-12-03 20:04:50 +0530

    third commit

--- amzon/models.py
+++ amzon/models.py
@@ -8,23 +8,3 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
-
-	@classmethod
-	def create_product(cls, name, price, description=""):
-		return cls.objects.create(name=name, price=price, description=description)
-
-	@classmethod
-	def get_product(cls, pk):
-		return cls.objects.get(pk=pk)
-
-	def update_product(self, **kwargs):
-		for key, value in kwargs.items():
-			if hasattr(self, key):
-				setattr(self, key, value)
-		self.save()
-		return self
-
-	def delete_product(self):
-		pk = self.pk
-		self.delete()
-		return pk
--- amzon/urls.py
+++ amzon/urls.py
@@ -2,12 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('products/', views.list_products, name='amzon-product-list'),
-	path('products/<int:pk>/', views.product_detail, name='amzon-product-detail'),
-	# Template / web views
-	path('web/products/', views.product_list_web, name='amzon-product-list-web'),
-	path('web/products/create/', views.product_create_web, name='amzon-product-create-web'),
-	path('web/products/<int:pk>/edit/', views.product_edit_web, name='amzon-product-edit-web'),
-	path('web/products/<int:pk>/delete/', views.product_delete_web, name='amzon-product-delete-web'),
 ]
 
